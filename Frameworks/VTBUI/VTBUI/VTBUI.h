8610a5fb792976e66c0bc82306f41f44a59d0eca
viktor.volkov <viktor.volkov@e-legion.com>
2020-10-09 22:31:35 +0300

    Initial iOS Commit

---
+++ Frameworks/VTBUI/VTBUI/VTBUI.h
@@ -0,0 +1,18 @@
+//
+//  VTBUI.h
+//  VTBUI
+//
+//  Created by viktor.volkov on 09.10.2020.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for VTBUI.
+FOUNDATION_EXPORT double VTBUIVersionNumber;
+
+//! Project version string for VTBUI.
+FOUNDATION_EXPORT const unsigned char VTBUIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VTBUI/PublicHeader.h>
+
+
